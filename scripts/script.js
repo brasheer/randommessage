6062f45e1baa68942fae2dcc321c982d0797e4e4
Aaron Trent <aatrent@gmail.com>
2023-01-14 20:10:59 -0600

    updated html and redid script

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,23 @@
-console.log('Hello World');
-const randomMessages = [
-"“All our dreams can come true, if we have the courage to pursue them.” —Walt Disney",
-"“The secret of getting ahead is getting started.” —Mark Twain",
-"“I’ve missed more than 9,000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life, and that is why I succeed.” —Michael Jordan",
-"“Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.” —Mary Kay Ash",
-"“The best time to plant a tree was 20 years ago. The second best time is now.” ―Chinese Proverb",
-"“Only the paranoid survive.” —Andy Grove",
-"“It’s hard to beat a person who never gives up.” —Babe Ruth",
-"“I wake up every morning and think to myself, ‘How far can I push this company in the next 24 hours.’” —Leah Busque",
-"“If people are doubting how far you can go, go so far that you can’t hear them anymore.” —Michele Ruiz",
-"“We need to accept that we won’t always make the right decisions, that we’ll screw up royally sometimes―understanding that failure is not the opposite of success, it’s part of success.” ―Arianna Huffington",
-"“Write it. Shoot it. Publish it. Crochet it. Sauté it. Whatever. MAKE.” —Joss Whedon"
+const firstPart = [
+    'You will',
+    'Your dog will',
+    'Your friend will',
+    'The random squirrel in the tree will',
+    'That guy whose face you hate will',
+]
+const secondPart = [
+    'jump over',
+    'fart in',
+    'slide under',
+    'piss on',
+    'steal from'
+]
+const thirdPart = [
+    'the snatchbox.',
+    'the clocktower.',
+    'the Tesla battery.',
+    'the meth house.',
+    'the red-light district.'
 ]
 
 const images = [
@@ -27,14 +34,23 @@ const images = [
 function returnRandom(inputArray){
     return inputArray[Math.floor(Math.random() * inputArray.length)];
 }
-/* const element = document.getElementById("generator");
-const node = document.createElement("h2");
-const textnode = document.createTextNode(returnRandom(randomMessages));
-node.appendChild(textnode);
 
-element.appendChild(node);
-element.setAttribute('style', `background-image: url('resources/img/${returnRandom(images)}')`); */
+function mixedMessages(array1, array2, array3){
+    let selection1 = returnRandom(array1);
+    let selection2 = returnRandom(array2);
+    let selection3 = returnRandom(array3);
+    let fullMessage = "";
+    fullMessage = `${selection1} ${selection2} ${selection3}`;
+    return fullMessage;
+}
 
+/*  function insertElement(modify:<string>, create:<string>, inputText:<string>, styleTag:<string>, styles:<string>) 
+ *  modify takes argument of html element to be modified, create takes argument of element to be created
+ *  inputText is the text that you want inside the tag being created, styleTag is supposed to be 'style' but some other
+ *  attribute could be used in its place.  Currently only handles single attributes.  styles should be any css styles
+ *  you wish to add to the parent element. Improvements could be made. Would like to add a check to prevent same image
+ *  image file from being used.
+ */
 function insertElement(modify, create, inputText, styleTag, styles){
     const element = document.getElementById(modify);
     const node = document.createElement(create);
@@ -50,11 +66,15 @@ function insertElement(modify, create, inputText, styleTag, styles){
     element.setAttribute(styleTag, styles);
 }
 
+/* Check boolean for first run, probably unnecessary */
 let hasRun = false;
-
+/* using the insertElement function in the webPage itself produced errors when trying to find image file names
+ * created helper function to execute the function from an onClick event, errors are gone.
+ */
 function clickMe(){
-    insertElement("generator", "h2", returnRandom(randomMessages), "style", `background-image: url('resources/img/${returnRandom(images)}')`);
+    insertElement("generator", "h2", mixedMessages(firstPart, secondPart, thirdPart), "style", `background-image: url('resources/img/${returnRandom(images)}')`);
 }
+//Runs clickMe() once for initial page load.
 if(hasRun === false){
     clickMe();
     hasRun = true;   
